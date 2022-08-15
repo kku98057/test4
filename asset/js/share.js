7b1476898688853a3fb88842d9bb613e1122c9ca
kku98057 <kku98057@naver.com>
2022-08-15 18:20:00 +0900

    소셜 테스트3

--- asset/js/share.js
+++ asset/js/share.js
@@ -6,49 +6,16 @@ function mainKakaoShare() {
     content: {
       title: "이상한 고래 성향 테스트",
       description: "나와 어울리는 고래는 무엇일까 ?",
-      imageUrl: "/asset/img/mainwhale.png",
+      imageUrl: "https://kku98057.github.io/test4/asset/img/mainwhale.png",
       link: {
         webUrl: "https://kku98057.github.io/test4/",
       },
     },
-    itemContent: {
-      profileText: "Kakao",
-      profileImageUrl:
-        "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
-      titleImageUrl:
-        "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
-      titleImageText: "Cheese cake",
-      titleImageCategory: "Cake",
-      items: [
-        {
-          item: "Cake1",
-          itemOp: "1000원",
-        },
-        {
-          item: "Cake2",
-          itemOp: "2000원",
-        },
-        {
-          item: "Cake3",
-          itemOp: "3000원",
-        },
-        {
-          item: "Cake4",
-          itemOp: "4000원",
-        },
-        {
-          item: "Cake5",
-          itemOp: "5000원",
-        },
-      ],
-      sum: "총 결제금액",
-      sumOp: "15000원",
-    },
-    social: {
-      likeCount: 10,
-      commentCount: 20,
-      sharedCount: 30,
-    },
+    // social: {
+    //   likeCount: 10,
+    //   commentCount: 20,
+    //   sharedCount: 30,
+    // },
     buttons: [
       {
         title: "나도 테스트 해보기!",
@@ -68,7 +35,7 @@ function resultKakaoShare() {
     content: {
       title: "이상한 고래 성향 테스트",
       description: "나와 어울리는 고래는 무엇일까 ?",
-      imageUrl: "/asset/img/mainwhale.png",
+      imageUrl: "https://kku98057.github.io/test4/asset/img/mainwhale.png",
       link: {
         webUrl: address,
       },
