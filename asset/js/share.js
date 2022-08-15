02a317b60a58bb8137216aa090f65533d6836a22
kku98057 <kku98057@naver.com>
2022-08-15 18:12:41 +0900

    소셜 테스트2

--- asset/js/share.js
+++ asset/js/share.js
@@ -1,3 +1,5 @@
+const mainAddress = `https://kku98057.github.io/test4/`;
+
 function mainKakaoShare() {
   Kakao.Share.sendDefault({
     objectType: "feed",
@@ -71,49 +73,22 @@ function resultKakaoShare() {
         webUrl: address,
       },
     },
-    itemContent: {
-      profileText: "Kakao",
-      profileImageUrl:
-        "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
-      titleImageUrl:
-        "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
-      titleImageText: "Cheese cake",
-      titleImageCategory: "Cake",
-      items: [
-        {
-          item: "Cake1",
-          itemOp: "1000원",
-        },
-        {
-          item: "Cake2",
-          itemOp: "2000원",
-        },
-        {
-          item: "Cake3",
-          itemOp: "3000원",
-        },
-        {
-          item: "Cake4",
-          itemOp: "4000원",
-        },
-        {
-          item: "Cake5",
-          itemOp: "5000원",
-        },
-      ],
-      sum: "총 결제금액",
-      sumOp: "15000원",
-    },
-    social: {
-      likeCount: 10,
-      commentCount: 20,
-      sharedCount: 30,
-    },
+    // social: {
+    //   likeCount: 10,
+    //   commentCount: 20,
+    //   sharedCount: 30,
+    // },
     buttons: [
+      {
+        title: "친구꺼 구경하기!",
+        link: {
+          webUrl: address,
+        },
+      },
       {
         title: "나도 테스트 해보기!",
         link: {
-          webUrl: address,
+          webUrl: mainAddress,
         },
       },
     ],
