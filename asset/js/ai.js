33ee34c6a8dbeb3e562d8168bca1e025adf4ade6
kku98057 <kku98057@naver.com>
2022-08-15 18:07:00 +0900

    소셜 테스트

--- asset/js/ai.js
+++ asset/js/ai.js
@@ -32,7 +32,6 @@ async function predict() {
   });
 
   //   key,text 추가
-  console.log(infoList);
 
   prediction.forEach((predic, index) => {
     predic.title = infoList[index].title;
@@ -77,6 +76,7 @@ async function predict() {
   const resultImg = document.querySelector("#resultImg");
   const imgSrc = `./asset/img/image-${maxNub.key}.png`;
   resultImg.src = imgSrc;
+  resultImg.alt = maxNub.key;
 
   // title,text
   // title
@@ -84,4 +84,79 @@ async function predict() {
   const resutlText = document.querySelector(".result_title_p3");
   resultTitle.innerHTML = `${maxTopPrediction[0].title}`;
   resutlText.innerHTML = `${maxTopPrediction[0].text}`;
+}
+
+const fileInput = document.querySelector("#upImg");
+const faceImg = document.querySelector(".uploadImg img");
+const upImgText = document.querySelector(".upImgText");
+const startBtn = document.querySelector(".upBtn");
+const uploadPage = document.querySelector(".monitor_img");
+const resultPage = document.querySelector(".result");
+
+const reader = new FileReader();
+
+function handleEvent(event) {
+  if (event.type === "load") {
+    faceImg.src = reader.result;
+  }
+}
+function addListeners(reader) {
+  reader.addEventListener("loadstart", handleEvent);
+  reader.addEventListener("load", handleEvent);
+  reader.addEventListener("loadend", handleEvent);
+  reader.addEventListener("progress", handleEvent);
+  reader.addEventListener("error", handleEvent);
+  reader.addEventListener("abort", handleEvent);
+}
+
+function handleSelected(e) {
+  const selectedFile = fileInput.files[0];
+  if (selectedFile) {
+    addListeners(reader);
+    reader.readAsDataURL(selectedFile);
+    upImgText.style.display = "block";
+    upImgText.innerHTML = "이미지를 인식 중 입니다.<br> 잠시만 기다려주세요.";
+    init().then(() => {
+      startBtn.style.display = "flex";
+      setTimeout(() => {
+        startBtn.style.transition = "opacity 0.3s";
+        startBtn.style.opacity = 1;
+      }, 500);
+
+      upImgText.innerHTML = "스타트 버튼을 클릭해주세요.";
+    });
+  }
+}
+
+fileInput.addEventListener("change", handleSelected);
+
+startBtn.addEventListener("click", startBtnHandler);
+
+function startBtnHandler() {
+  upImgText.innerHTML = `데이터를 받아오고 있습니다.<br> 잠시만 기다려주세요`;
+  setTimeout(() => {
+    predict();
+    uploadPage.style.opacity = 0;
+    setTimeout(() => {
+      uploadPage.style.display = "none";
+    }, 500);
+
+    resultPage.style.display = "block";
+    setTimeout(() => {
+      resultPage.style.opacity = 1;
+    }, 500);
+  }, 300);
+}
+// genderBtn
+const genderBtn = document.querySelector(".gender_btn");
+const genderToggle = document.querySelector(".gender_btn_toggle");
+let gender = "man";
+genderBtn.addEventListener("click", genderBtnHandler);
+
+function genderBtnHandler() {
+  const gender = document.querySelectorAll(".gender_img");
+  genderToggle.classList.toggle("active");
+  gender.forEach((gender) => {
+    gender.classList.toggle("active");
+  });
 }
---
+++ asset/js/share.js
@@ -0,0 +1,121 @@
+function mainKakaoShare() {
+  Kakao.Share.sendDefault({
+    objectType: "feed",
+    content: {
+      title: "이상한 고래 성향 테스트",
+      description: "나와 어울리는 고래는 무엇일까 ?",
+      imageUrl: "/asset/img/mainwhale.png",
+      link: {
+        webUrl: "https://kku98057.github.io/test4/",
+      },
+    },
+    itemContent: {
+      profileText: "Kakao",
+      profileImageUrl:
+        "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+      titleImageUrl:
+        "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+      titleImageText: "Cheese cake",
+      titleImageCategory: "Cake",
+      items: [
+        {
+          item: "Cake1",
+          itemOp: "1000원",
+        },
+        {
+          item: "Cake2",
+          itemOp: "2000원",
+        },
+        {
+          item: "Cake3",
+          itemOp: "3000원",
+        },
+        {
+          item: "Cake4",
+          itemOp: "4000원",
+        },
+        {
+          item: "Cake5",
+          itemOp: "5000원",
+        },
+      ],
+      sum: "총 결제금액",
+      sumOp: "15000원",
+    },
+    social: {
+      likeCount: 10,
+      commentCount: 20,
+      sharedCount: 30,
+    },
+    buttons: [
+      {
+        title: "나도 테스트 해보기!",
+        link: {
+          webUrl: "https://kku98057.github.io/test4/",
+        },
+      },
+    ],
+  });
+}
+function resultKakaoShare() {
+  const nowPage = resultImg.alt;
+  const address = `https://kku98057.github.io/test4/page_${nowPage}.html`;
+
+  Kakao.Share.sendDefault({
+    objectType: "feed",
+    content: {
+      title: "이상한 고래 성향 테스트",
+      description: "나와 어울리는 고래는 무엇일까 ?",
+      imageUrl: "/asset/img/mainwhale.png",
+      link: {
+        webUrl: address,
+      },
+    },
+    itemContent: {
+      profileText: "Kakao",
+      profileImageUrl:
+        "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+      titleImageUrl:
+        "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+      titleImageText: "Cheese cake",
+      titleImageCategory: "Cake",
+      items: [
+        {
+          item: "Cake1",
+          itemOp: "1000원",
+        },
+        {
+          item: "Cake2",
+          itemOp: "2000원",
+        },
+        {
+          item: "Cake3",
+          itemOp: "3000원",
+        },
+        {
+          item: "Cake4",
+          itemOp: "4000원",
+        },
+        {
+          item: "Cake5",
+          itemOp: "5000원",
+        },
+      ],
+      sum: "총 결제금액",
+      sumOp: "15000원",
+    },
+    social: {
+      likeCount: 10,
+      commentCount: 20,
+      sharedCount: 30,
+    },
+    buttons: [
+      {
+        title: "나도 테스트 해보기!",
+        link: {
+          webUrl: address,
+        },
+      },
+    ],
+  });
+}
